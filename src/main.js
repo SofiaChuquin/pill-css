42828d96e5c4aa1962a5d4b8f8ecd048a558e2a9
ivy <unjust@gmail.com>
2021-06-24 11:21:10 -0500

    agrega funcion a renderData

--- src/main.js
+++ src/main.js
@@ -4,3 +4,8 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+const renderData = () => {
+  var newData = '';
+  console.log(newData);
+}
