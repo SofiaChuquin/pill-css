2e5e5e98b0e61fdc898c6a95f26f222b3955774f
genesismauries <exodo_54@hotmail.com>
2021-06-24 11:36:27 -0500

    Crea fn getData

--- src/main.js
+++ src/main.js
@@ -4,3 +4,8 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+const getData=()=>{
+    var newData = "";
+    console.log(newData)
+}
