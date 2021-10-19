5124b972f0554dca9bc9ca416da61e30c53ef39c
ivy <unjust@gmail.com>
2021-10-19 08:33:33 -0500

    clean main

--- src/main.js
+++ src/main.js
@@ -1,15 +1,2 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-const getData = () => {
-    let newData = "";
-    const year = 2021;
-    if(year == 2021){
-        console.log("SI");
-    }else{
-        console.log("no");
-	}
-	return newData;
-}
+console.log('hello world');
